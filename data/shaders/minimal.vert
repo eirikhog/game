7b9030a8d73e05b7df109493dffa358fa2a73c5e
Eirik Hoegmo <eirikhog@gmail.com>
2015-12-22 21:22:19 +0100

    Bouncing rectangle and scaling image.

--- data/shaders/minimal.vert
+++ data/shaders/minimal.vert
@@ -15,8 +15,16 @@ void main(void)
 	mat4 projectionMatrix = mat4( 2.0/size.x, 0.0, 0.0, -1.0,
                               0.0, -2.0/size.y, 0.0, 1.0,
                               0.0, 0.0, 1, 0,
-                              0.0, 0.0, 0.0, 1.0);    
+                              0.0, 0.0, 0.0, 1.0);
+
+    mat4 scaleMatrix = mat4( size.x / 1920.0, 0.0, 0.0, 0.0,
+                             0.0, size.y / 1080.0, 0.0, 0.0,
+                             0.0, 0.0, 1.0, 0.0,
+                             0.0, 0.0, 0.0, 1.0);
+
 	gl_Position = vec4(in_Position, 1.0);
+    gl_Position *= scaleMatrix;
     gl_Position *= projectionMatrix;
+    // gl_Position *= scaleMatrix;
 	ex_Color = in_Color;
 }
--- src/Game.cpp
+++ src/Game.cpp
@@ -19,22 +19,29 @@ void EXPORT UpdateGame(platform_api *api, game_memory *memory, game_input *input
         ctx = initialize_renderer(api);
         state->initialized = true;
     }
+    static v2 acc = { 5, 7 };
+    const v2 screen_size = { 1920.0f, 1080.0f };
+    pos.x += acc.x;
+    if (pos.x + 100 > screen_size.x || pos.x < 0) {
+        acc.x *= -1;
+    }
 
-    pos.x += 2.5f;
-    if (pos.x > 1200) pos.x = 0;
-    pos.y += 3.13f;
-    if (pos.y > 700) pos.y = 0;
+    pos.y += acc.y;
+    if (pos.y + 100 > screen_size.y || pos.y < 0) {
+        acc.y *= -1;
+    }
 
     render_start(&ctx);
 
+
     color cornflower_blue = { 0.392156862745098f, 0.5843137254901961f, 0.9294117647058824f };
-    render_rect(&ctx, 0, 0, 1350, 750, cornflower_blue);
+    render_rect(&ctx, 0, 0, screen_size.x, screen_size.y, cornflower_blue);
 
     color c1 = { 1.0f, 1.0f, 1.0f, 1.0f };
     render_rect(&ctx, (int32)pos.x, (int32)pos.y, 100, 100, c1);
 
-    color c2 = { 0.0f, 1.0f, 0.0f, 1.0f };
-    render_rect(&ctx, (int32)pos.x + 75, (int32)pos.y + 75, 60, 60, c2);
+    //color c2 = { 0.0f, 1.0f, 0.0f, 1.0f };
+    //render_rect(&ctx, (int32)pos.x + 75, (int32)pos.y + 75, 60, 60, c2);
 
     render_end(&ctx);
 }
