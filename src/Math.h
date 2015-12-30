3b1aa7d35e1f9d3fe52e99b6125403418b85fc66
Eirik Hoegmo <eirikhog@gmail.com>
2015-12-30 12:30:15 +0100

    Fixed data types and added missing operator.

--- src/Math.h
+++ src/Math.h
@@ -1,15 +1,24 @@
 #pragma once
 
+#include "Common.h"
+
 typedef struct {
-    float x;
-    float y;
+    real32 x;
+    real32 y;
 } v2;
 
 typedef struct {
-    float x;
-    float y;
-    float z;
+    real32 x;
+    real32 y;
+    real32 z;
 } v3;
+
+typedef struct {
+    real32 x;
+    real32 y;
+    real32 width;
+    real32 height;
+} rect;
 
 inline v2 operator+(const v2 left, const v2 right) {
     v2 result = { left.x + right.x, left.y + right.y };
@@ -25,3 +34,9 @@ inline v2& operator+=(v2 &left, const v2 right) {
     left = left + right;
     return left;
 }
+
+inline v2& operator-=(v2 &left, const v2 right) {
+    left = left - right;
+    return left;
+}
+
