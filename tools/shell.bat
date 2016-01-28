c2ff0190871ffc8ef312a8d82809562b24b41b65
Eirik Hoegmo <eirikhog@gmail.com>
2016-01-28 11:20:10 +0100

    Fixed vim bat, and added makefile path to shell.

--- tools/shell.bat
+++ tools/shell.bat
@@ -2,8 +2,10 @@
 
 echo Setting up environment...
 
+set MAKEPATH=c:\MinGW\msys\1.0\bin
+
 call "%programfiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
-set PATH=%~dp0;%PATH%
+set PATH=%~dp0;%MAKEPATH%;%PATH%
 
 echo Done!
 
--- tools/vim.bat
+++ tools/vim.bat
@@ -1,3 +1,3 @@
 @echo off
 
-"C:\Program Files (x86)\Vim\vim74\gvim.exe"
+start "" "C:\Program Files (x86)\Vim\vim74\gvim.exe"
