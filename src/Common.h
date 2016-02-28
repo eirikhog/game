c0ca80b15cd3c14b905b3ccbbf84cd2fdc733ef4
Eirik Hoegmo <eirikhog@gmail.com>
2016-02-28 12:56:15 +0100

    Added shorter typedefs for value types.

--- src/Common.h
+++ src/Common.h
@@ -7,6 +7,20 @@
 #include <string.h>
 #include <stdio.h>
 
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+
+typedef float r32;
+typedef double r64;
+
+// TODO: Depricate and remove these from the code.
 typedef uint8_t uint8;
 typedef uint16_t uint16;
 typedef uint32_t uint32;
@@ -18,7 +32,7 @@ typedef int32_t int32;
 typedef int64_t int64;
 
 typedef float real32;
-typedef float real64;
+typedef double real64;
 
 typedef uint32 bool32;
 
