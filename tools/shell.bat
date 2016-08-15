b81acf8d8d009a474baefd261549e98fe156c0ff
Eirik Hoegmo <eirikhog@gmail.com>
2016-08-15 20:58:09 +0200

    Use 64-bit toolchain from command line.

--- tools/shell.bat
+++ tools/shell.bat
@@ -4,7 +4,7 @@ echo Setting up environment...
 
 set MAKEPATH=c:\MinGW\msys\1.0\bin
 
-call "%programfiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+call "%programfiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 set PATH=%~dp0;%MAKEPATH%;%PATH%
 
 echo Done!
